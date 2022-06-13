885fb9b550b2a339046634e1c91e6c84ef68a621
Samuel Stalschus <samuel5stalschus@gmail.com>
2022-06-12 23:52:52 -0300

    Feat: Add create user flow

--- cmd/api/core/usecase/create_user.go
+++ cmd/api/core/usecase/create_user.go
@@ -1,16 +1,32 @@
 package usecase
 
+import (
+	"log"
+	"social-go/cmd/api/core/model"
+	"social-go/cmd/api/persistence"
+)
+
 type CreateUserUseCase interface {
 	Execute(user string) string
 }
 
-type CreateUser struct{}
-
-func NewCreateUser() *CreateUser {
-	return &CreateUser{}
+type CreateUser struct {
+	userDao persistence.UserDao
 }
 
-func (createUser *CreateUser) Execute(user string) string {
-	string := "User created"
-	return string
+func NewCreateUser(userDao persistence.UserDao) *CreateUser {
+	return &CreateUser{
+		userDao: userDao,
+	}
+}
+
+func (createUser *CreateUser) Execute(user model.User) (*model.User, error) {
+
+	userCreated, err := createUser.userDao.Save(&user)
+
+	if err != nil {
+		log.Fatal("Error to create a new user")
+	}
+
+	return userCreated, nil
 }
